260e7531d53b2c6dba2443172df002dfe5c863b6
jperkins01 <jaredperkins2001@gmail.com>
2020-02-06 14:59:18 -0500

    Added min and max variable

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -7,13 +7,16 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to number wizard dude");
         // Used for introduction to game
         Debug.Log("Pick an number, dont tell me what it is");
         // Tells the user basic instruction for game
-        Debug.Log("The highest number you can pick is 1000");
+        Debug.Log("The highest number you can pick is: " + max);
         // The user can not pick a number greater than 1000
-        Debug.Log("The lowest number you can pick is 1");
+        Debug.Log("The lowest number you can pick is: " + min);
         // The user can not pick a number less than 1
     }
 
