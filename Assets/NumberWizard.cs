7aabf868d204749aff72f45e652f5e983bcd6d25
jperkins01 <jaredperkins2001@gmail.com>
2020-02-06 15:38:12 -0500

    add min = guess

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NumberWizard : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class NumberWizard : MonoBehaviour { 
         int max = 1000;
         // sets max value to 1000
         int min = 1;
         // sets min value to 1
+        int guess = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
 
         Debug.Log("Welcome to number wizard dude");
         // Used for introduction to game
@@ -31,14 +33,19 @@ public class NumberWizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("Up Arrow key was pressed");
+            // used to use up arrow 
+            min = guess;
+            Debug.Log(guess);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down Arrow key was pressed");
+            // used to use down arrow
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("You hit Enter");
+            // used to use enter key
         }
     }
 
